ef11af36b83ffa889eca720ef3848cb17ad3907f
Shabi96 <vuvivokiller@gmail.com>
2023-05-01 17:48:59 +0700

    Fix

--- src/thuat_toan/CountElementInString.java
+++ src/thuat_toan/CountElementInString.java
@@ -2,20 +2,20 @@ package thuat_toan;
 
 import java.util.Scanner;
 
-public class CountCharInString {
+public class CountElementInString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập vào 1 chuỗi: ");
         String str = sc.nextLine();
-        String[] arr = str.split("");
-        System.out.println("Nhập vào 1 kí tự bất kì: ");
+        String[] arr = str.split(" ");
+        System.out.println("Nhập vào 1 từ bất kì: ");
         String c = sc.nextLine();
         int count = 0;
         for (String s : arr) {
-            if (c.equals(s)) {
+            if (s.equals(c)) {
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Số lần xuất hiện của phần tử " + "'" + c + "'" + " là " + count);
     }
 }
